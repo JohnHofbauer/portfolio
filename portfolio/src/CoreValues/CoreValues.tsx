16b44c2456c1eb583ee110ba3311d5de80c62859
JohnHofbauer <98779922+JohnHofbauer@users.noreply.github.com>
2023-06-09 14:08:50 -0400

    updated corevalues

--- portfolio/src/CoreValues/CoreValues.tsx
+++ portfolio/src/CoreValues/CoreValues.tsx
@@ -1,115 +1,87 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./CoreValues.scss";
+import WordCloud from "wordcloud";
 
 export default function CoreValues() {
+  useEffect(() => {
+    const container = document.getElementById("wordcloud-container");
+    if (container) {
+      WordCloud(container, {
+        gridSize: 8,
+        weightFactor: 16,
+        fontFamily: "Hiragino Mincho Pro, serif",
+        color: "random-dark",
+        backgroundColor: "#1a1a1a",
+        rotateRatio: 0,
+        list: [
+          ["Integrity", 5],
+          ["Excellence", 2],
+          ["Innovation", 3],
+          ["Teamwork", 6],
+          ["Respect", 4],
+          ["Adaptability", 2],
+          ["Sustainability", 1],
+          ["Transparency", 1],
+          ["Diversity and Inclusion", 4],
+        ],
+      });
+    }
+  }, []);
+
+  let examples = [
+    {
+      title: "Integrity",
+      description:
+        "The adherence to a strong moral or ethical code; honesty and fairness in all actions.",
+    },
+    {
+      title: "Excellence",
+      description:
+        "The pursuit of the highest standards of quality and performance in all endeavors.",
+    },
+    {
+      title: "Innovation",
+      description:
+        "The ability to think creatively and bring new ideas to the table in order to improve processes, products, and services.",
+    },
+    {
+      title: "Teamwork",
+      description:
+        "The collaboration and cooperation among individuals and teams to achieve a common goal.",
+    },
+    {
+      title: "Respect",
+      description:
+        "The recognition and consideration of the worth, rights, and dignity of all individuals.",
+    },
+    {
+      title: "Adaptability",
+      description:
+        "The ability to change and adjust in response to new information, situations, or environments.",
+    },
+    {
+      title: "Sustainability",
+      description:
+        "The ability to meet the needs of the present without compromising the ability of future generations to meet their own needs.",
+    },
+    {
+      title: "Transparency",
+      description:
+        "The practice of being open and honest in communication and decision-making.",
+    },
+    {
+      title: "Diversity and Inclusion",
+      description: "The recognition and valuing of differences among people.",
+    },
+  ];
   return (
     <>
       <div className="section1">
         <h3 className="top-heading">Core Values</h3>
-        <div className="center">
-          <table>
-            <tbody>
-              <tr style={{ height: "100px" }}>
-                <td style={{ height: "100px", width: "130px" }}>
-                  <h4>Integrity</h4>
-                </td>
-                <td style={{ height: "100px", width: "400px" }}>
-                  <p>
-                    The adherence to a strong moral or ethical code; honesty and
-                    fairness in all actions.
-                  </p>
-                </td>
-              </tr>
-              <tr style={{ height: "100px" }}>
-                <td style={{ height: "100px", width: "130px" }}>
-                  <h4>Excellence</h4>
-                </td>
-                <td style={{ height: "100px", width: "400px" }}>
-                  <p>
-                    The pursuit of the highest standards of quality and
-                    performance in all endeavors.
-                  </p>
-                </td>
-              </tr>
-              <tr style={{ height: "100px" }}>
-                <td style={{ height: "100px", width: "130px" }}>
-                  <h4>Innovation</h4>
-                </td>
-                <td style={{ height: "100px", width: "400px" }}>
-                  The ability to think creatively and bring new ideas to the
-                  table in order to improve processes, products, and services.
-                </td>
-              </tr>
-              <tr style={{ height: "100px" }}>
-                <td style={{ height: "100px", width: "130px" }}>
-                  <h4>Teamwork</h4>
-                </td>
-                <td style={{ height: "100px", width: "400px" }}>
-                  <p>
-                    The collaboration and cooperation among individuals and
-                    teams to achieve a common goal.
-                  </p>
-                </td>
-              </tr>
-              <tr style={{ height: "100px" }}>
-                <td style={{ height: "100px", width: "130px" }}>
-                  <h4>Respect</h4>
-                </td>
-                <td style={{ height: "100px", width: "400px" }}>
-                  <p>
-                    The recognition and consideration of the worth, rights, and
-                    dignity of all individuals.
-                  </p>
-                </td>
-              </tr>
-              <tr style={{ height: "100px" }}>
-                <td style={{ height: "100px", width: "130px" }}>
-                  <h4>Adaptability</h4>
-                </td>
-                <td style={{ height: "100px", width: "400px" }}>
-                  <p>
-                    The ability to change and adjust in response to new
-                    information, situations, or environments.
-                  </p>
-                </td>
-              </tr>
-              <tr style={{ height: "100px" }}>
-                <td style={{ height: "100px", width: "130px" }}>
-                  <h4>Sustainability</h4>
-                </td>
-                <td style={{ height: "100px", width: "400px" }}>
-                  <p>
-                    The ability to meet the needs of the present without
-                    compromising the ability of future generations to meet their
-                    own needs.
-                  </p>
-                </td>
-              </tr>
-              <tr style={{ height: "100px" }}>
-                <td style={{ height: "100px", width: "130px" }}>
-                  <h4>Transparency</h4>
-                </td>
-                <td style={{ height: "100px", width: "400px" }}>
-                  <p>
-                    The practice of being open and honest in communication and
-                    decision-making.
-                  </p>
-                </td>
-              </tr>
-              <tr style={{ height: "100px" }}>
-                <td style={{ height: "100px", width: "130px" }}>
-                  <h4>Diversity and Inclusion</h4>
-                </td>
-                <td style={{ height: "100px", width: "400px" }}>
-                  <p>
-                    The recognition and valuing of differences among people.
-                  </p>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        <div className="center"></div>
+        <div
+          style={{ height: "600px", width: "600px", margin: "auto" }}
+          id="wordcloud-container"
+        ></div>
       </div>
     </>
   );
