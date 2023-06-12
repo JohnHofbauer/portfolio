18e907fea7b9452e8ebc7aa5a7e80e77f8fdfc21
JohnHofbauer <98779922+JohnHofbauer@users.noreply.github.com>
2023-06-12 08:46:33 -0400

    fix images

--- src/components/BackgrooundAnimation/BackgroundAnimation.js
+++ src/components/BackgrooundAnimation/BackgroundAnimation.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Img } from "./BackgrounStyles";
 
-const BackgroundAnimation = () => <Img src="/images/my_Torus.png" />;
+const BackgroundAnimation = () => <Img src="images/my_Torus.png" />;
 
 export default BackgroundAnimation;
